ee47ff143a9841b5fdf429e1d084a5f841af993f
Ecnavda <ecnavda@gmail.com>
2020-04-23 15:35:53 -0400

    Now scrapes and generates for the ions as well.

--- scrape.py
+++ scrape.py
@@ -89,7 +89,72 @@ def parseIons(page: str):
         # first cation at [2] (Aluminum) to [26] (Zinc)
         cationsSimple = cationTable.find_all("tr")[2:26]
         # Polyatomic from [28] - [30]
-        cationsPoly = cationTable.find_all("tr")[28:30]
+        # No uniformity with polyatomic cations html
+        #cationsPoly = cationTable.find_all("tr")[28:31]
+        anionsSimple = anionTable.find_all("tr")[3:13]
+
+        ions = []
+        for row in cationsSimple:
+            details = row.find_all("td")
+            name = details[0].string
+            abbr = details[1].contents[0].string
+            charge = details[1].contents[1].string
+            if charge == "+":
+                charge = "1"
+            else:
+                charge = charge.replace("+", "")
+            ionType = "cation"
+            ions.append(Ion(abbr, name, charge, ionType))
+        for row in anionsSimple:
+            details = row.find_all("td")
+            name = details[0].string
+            abbr = details[1].contents[0].string
+            charge = details[1].contents[1].string
+            if charge == "−":
+                charge = "1"
+            else:
+                charge = charge.replace("−", "")
+            ionType = "anion"
+            ions.append(Ion(abbr, name, charge, ionType))
+        # Polyatomic failure section :-(
+        ions.append(Ion("NH4", "Ammonium", "1", "cation"))
+        ions.append(Ion("H3O", "Hydronium", "1", "cation"))
+        ions.append(Ion("Hg2", "Mercury(I)", "2", "cation"))
+        ions.append(Ion("CH3COO", "Acetate", "1", "anion"))
+        ions.append(Ion("N3", "Azide", "1", "anion"))
+        ions.append(Ion("HCOO", "Formate", "1", "anion"))
+        ions.append(Ion("C2O4", "Oxalate", "2", "anion"))
+        ions.append(Ion("CN", "Cyanide", "1", "anion"))
+        ions.append(Ion("CO3", "Carbonate", "2", "anion"))
+        ions.append(Ion("CIO3", "Chlorate", "1", "anion"))
+        ions.append(Ion("CrO4", "Chromate", "2", "anion"))
+        ions.append(Ion("Cr2O7", "Dichromate", "2", "anion"))
+        ions.append(Ion("H2PO4", "Dihydrogen Phosphate", "1", "anion"))
+        ions.append(Ion("HCO3", "Hydrogen Carbonate", "1", "anion"))
+        ions.append(Ion("HSO4", "Hydrogen Sulfate", "1", "anion"))
+        ions.append(Ion("HSO3", "Hydrogen Sulfite", "1", "anion"))
+        ions.append(Ion("OH", "Hydroxide", "1", "anion"))
+        ions.append(Ion("CIO", "Hypochlorite", "1", "anion"))
+        ions.append(Ion("HPO4", "Monohydrogen Phosphate", "2", "anion"))
+        ions.append(Ion("NO3", "Nitrate", "1", "anion"))
+        ions.append(Ion("NO2", "Nitrite", "1", "anion"))
+        ions.append(Ion("CIO4", "Perchlorate", "1", "anion"))
+        ions.append(Ion("MnO4", "Permanganate", "1", "anion"))
+        ions.append(Ion("O2", "Peroxide", "2", "anion"))
+        ions.append(Ion("PO4", "Phosphate", "3", "anion"))
+        ions.append(Ion("SO4", "Sulfate", "2", "anion"))
+        ions.append(Ion("SO3", "Sulfite", "2", "anion"))
+        ions.append(Ion("O2", "Superoxide", "1", "anion"))
+        ions.append(Ion("S2O3", "Thiosulfate", "2", "anion"))
+        ions.append(Ion("SiO4", "Silicate", "4", "anion"))
+        ions.append(Ion("SiO3", "Metasilicate", "2", "anion"))
+        ions.append(Ion("AlSiO4", "Aluminum Silicate", "1", "anion"))
+        ions.append(Ion("CH3COO", "Acetate", "1", "anion"))
+        
+        return ions
+
+    else:
+        return [Ion.error()]
 
     
 def generateElementsSQL(elements: List[Element]):
@@ -108,6 +173,18 @@ def generateElementsSQL(elements: List[Element]):
         return "Error"
 
 
+def generateIonsSQL(ions: List[Ion]):
+    if ions[0].name != "Error":
+        sql = "INSERT INTO ions (abbreviation, name, charge, type)\nVALUES\n"
+        for i in ions:
+            sql += f"('{i.abbr}', '{i.name}', {i.charge}, '{i.ionType}'),\n"
+        sql = sql[:-2] + ";"
+
+        return sql
+    else:
+        return "Error"
+
+
 def main():
     elementPageResult = getPage("https://en.wikipedia.org/wiki/List_of_chemical_elements")
     configPageResult = getPage("https://sciencenotes.org/list-of-electron-configurations-of-elements/")
@@ -118,8 +195,10 @@ def main():
     
     with open("elements.sql", '+w') as f:
         f.write(generateElementsSQL(elementsResult))
+
+    with open("ions.sql", '+w') as f:
+        f.write(generateIonsSQL(ionsResult))
    
 
 if __name__ == "__main__":
-    ionPageResult = getPage("https://en.wikipedia.org/wiki/Ion")
-    parseIons(ionPageResult)
+    main()
